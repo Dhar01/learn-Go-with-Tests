2455f7918858b9051a07affd2ba66666433bf390
Loknath Dhar <loknath-dhar@pm.me>
2023-08-16 16:13:07 +0600

    Added practice problems

--- 03_iteration/repeat.go
+++ 03_iteration/repeat.go
@@ -1,8 +1,12 @@
 package iteration
 
-func Repeat(character string) (repeated string) {
-	for i := 0; i < 5; i++ {
-		repeated = repeated + character
+func Repeat(character string, count int) (repeated string) {
+	if count == 0 {
+		count = 1
+	}
+
+	for i := 0; i < count; i++ {
+		repeated += character
 	}
 
 	return
--- 03_iteration/repeat_test.go
+++ 03_iteration/repeat_test.go
@@ -1,12 +1,39 @@
 package iteration
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestRepeat(t *testing.T) {
-	repeated := Repeat("a")
-	expected := "aaaaa"
+	t.Run("specific counts", func(t *testing.T) {
+		repeated := Repeat("a", 8)
+		expected := "aaaaaaaa"
+		assertCorrectMessage(t, repeated, expected)
+	})
+	t.Run("if count is zero", func(t *testing.T) {
+		repeated := Repeat("a", 0)
+		expected := "a"
+		assertCorrectMessage(t, repeated, expected)
+	})
+}
+
+func assertCorrectMessage(t testing.TB, repeated, expected string) {
+	t.Helper()
 
 	if repeated != expected {
 		t.Errorf("expected %q but got %q", expected, repeated)
+	}
+}
+
+func ExampleRepeat() {
+	result := Repeat("a", 5)
+	fmt.Println(result)
+	// Output: aaaaa
+}
+
+func BenchmarkRepeat(b *testing.B) {
+	for i := 1; i < b.N; i++ {
+		Repeat("a", 8)
 	}
 }
