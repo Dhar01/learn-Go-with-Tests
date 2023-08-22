b92296b028ae2f3863ab5dbcb8bef8c0d8e03c5d
Loknath Dhar <loknath-dhar@pm.me>
2023-08-22 20:57:23 +0600

    slices test

--- 04_arrays_slices/sum_test.go
+++ 04_arrays_slices/sum_test.go
@@ -1,6 +1,9 @@
 package main
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 func TestSum(t *testing.T) {
 	t.Run("collection of 5 numbers", func(t *testing.T) {
@@ -15,6 +18,15 @@ func TestSum(t *testing.T) {
 		want := 6
 		assertCorrectMessage(t, got, want, numbers)
 	})
+}
+
+func TestSumAll(t *testing.T) {
+	got := SumAll([]int{1, 2}, []int{0, 9})
+	want := []int{3, 9}
+
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("got %v want %v", got, want)
+	}
 }
 
 func assertCorrectMessage(t testing.TB, got, want int, numbers []int) {
