7cfa0d658889314c462fc75919b1adb8e158e5e9
Loknath Dhar <loknath-dhar@pm.me>
2023-08-23 00:43:58 +0600

    Struct and method working

--- 05_structs_methods_interfaces/shape_test.go
+++ 05_structs_methods_interfaces/shape_test.go
@@ -3,21 +3,31 @@ package shape
 import "testing"
 
 func TestPerimeter(t *testing.T) {
-	got := Perimeter(10.0, 10.0)
+	rectangle := Rectangle{Width: 10.0, Height: 10.0}
+	got := Perimeter(rectangle)
 	want := 40.0
 	assertCorrectMessage(t, got, want)
 }
 
 func TestArea(t *testing.T) {
-	got := Area(12.0, 6.0)
-	want := 72.0
-	assertCorrectMessage(t, got, want)
+	t.Run("rectangles", func(t *testing.T) {
+		rectangle := Rectangle{Height: 12.0, Width: 6.0}
+		got := rectangle.Area()
+		want := 72.0
+		assertCorrectMessage(t, got, want)
+	})
+	t.Run("circles", func(t *testing.T) {
+		circle := Circle{10}
+		got := circle.Area()
+		want := 314.1592653589793
+		assertCorrectMessage(t, got, want)
+	})
 }
 
 func assertCorrectMessage(t testing.TB, got, want float64) {
 	t.Helper()
 
 	if got != want {
-		t.Errorf("got %0.2f want %0.2f", got, want)
+		t.Errorf("got %g want %g", got, want)
 	}
 }
