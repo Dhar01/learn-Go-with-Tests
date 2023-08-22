c832166f583e0e834911af8d930599485a0728aa
Loknath Dhar <loknath-dhar@pm.me>
2023-08-22 11:47:33 +0600

    Using slices and refactored

--- 04_arrays_slices/sum_test.go
+++ 04_arrays_slices/sum_test.go
@@ -3,11 +3,22 @@ package main
 import "testing"
 
 func TestSum(t *testing.T) {
-	numbers := [5]int{1, 2, 3, 4, 5}
+	t.Run("collection of 5 numbers", func(t *testing.T) {
+		numbers := []int{1, 2, 3, 4, 5}
+		got := Sum(numbers)
+		want := 15
+		assertCorrectMessage(t, got, want, numbers)
+	})
+	t.Run("collection of any size", func(t *testing.T) {
+		numbers := []int{1, 2, 3}
+		got := Sum(numbers)
+		want := 6
+		assertCorrectMessage(t, got, want, numbers)
+	})
+}
 
-	got := Sum(numbers)
-	want := 15
-
+func assertCorrectMessage(t testing.TB, got, want int, numbers []int) {
+	t.Helper()
 	if got != want {
 		t.Errorf("got %d want %d given, %v", got, want, numbers)
 	}
